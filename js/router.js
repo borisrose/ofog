da215ff3284e66391a45092ceb4a21ad25e77f13
boralex <boris.alexandre.rose@gmail.com>
2023-06-21 10:58:38 +0200

    "fix/routing"

--- js/router.js
+++ js/router.js
@@ -24,20 +24,22 @@ const routes = [
 
 export const router = () => {
 
-    console.log('window.location', window.location)
-    if(window.location.pathname === routes[0].path){
+    console.log('window.location', window.location.pathname);
+
+    if(window.location.pathname === routes[0].path || window.location.pathname === "/ofog/"){
 
        const home = new Home()
        home.init()
     }
 
-    if(window.location.pathname === routes[1].path){
+    if(window.location.pathname === routes[1].path || window.location.pathname.endsWith('products.html/')){
 
+    
         const products = new Products()
         products.init()
     }
 
-    if(window.location.pathname === routes[2].path){
+    if(window.location.pathname === routes[2].path || window.location.pathname.endsWith('basket.html/')){
 
         const basket = new Basket()
         basket.init()
